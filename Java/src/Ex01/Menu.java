6b06399dbfbfc82e5cc97696261cd42ff13fd4d0
GiovanniRaf <giovanni.mota01@fatec.sp.gov.br>
2020-12-01 12:24:05 -0300

    Instancia de teste

--- Java/src/Ex01/Menu.java
+++ Java/src/Ex01/Menu.java
@@ -2,6 +2,7 @@ package Ex01;
 
 public class Menu {
     public static void main(String[] args) {
-
+        Horista maluko01 = new Horista("Jorge", "Cleberson", "123.456.789.00", 25.00, 150);
+        System.out.println("Nome completo: " + maluko01.getNome() + " " + maluko01.getSobrenome());
     }
 }
