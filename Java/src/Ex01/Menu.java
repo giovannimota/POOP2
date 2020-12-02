7efc0329d7a3cbb84eb7678aae7abfdb588e918a
GiovanniRaf <giovanni.mota01@fatec.sp.gov.br>
2020-12-02 16:08:47 -0300

    Novas instancias

--- Java/src/Ex01/Menu.java
+++ Java/src/Ex01/Menu.java
@@ -2,7 +2,14 @@ package Ex01;
 
 public class Menu {
     public static void main(String[] args) {
-        Horista maluko01 = new Horista("Jorge", "Cleberson", "123.456.789.00", 25.00, 150);
-        System.out.println("Nome completo: " + maluko01.getNome() + " " + maluko01.getSobrenome());
+
+        int[] folhaPagamento;
+
+        Assalariado malukoAssalariado01 = new Assalariado("Tadeu", "Silva", "123.456.789-00", 1500.00);
+        Horista malukoHorista01 = new Horista("Jorge", "Cleberson", "123.456.789.11", 25.00, 150);
+        Comissionado malukoComissionado01 = new Comissionado("Valter", "Oliveira", "123.456.789.22", 25000.00,10);
+
+
+        System.out.println("Nome completo: " + malukoHorista01.getNome() + " " + malukoHorista01.getSobrenome());
     }
 }
