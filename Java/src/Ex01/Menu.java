ad1b216a72de3c264b0da2894475c602ca434705
GiovanniRaf <giovanni.mota01@fatec.sp.gov.br>
2020-12-02 18:18:25 -0300

    Correcoes de atributo na fatura e instancia da fatura

--- Java/src/Ex01/Menu.java
+++ Java/src/Ex01/Menu.java
@@ -9,6 +9,7 @@ public class Menu {
         Horista malukoHorista01 = new Horista("Jorge", "Cleberson", "123.456.789.11", 25.00, 150);
         Comissionado malukoComissionado01 = new Comissionado("Valter", "Oliveira", "123.456.789.22", 25000.00,10);
 
+        Fatura divida01 = new Fatura("0123", "WaterCooler", 1, 250.00);
 
         System.out.println("Nome completo: " + malukoHorista01.getNome() + " " + malukoHorista01.getSobrenome());
     }
